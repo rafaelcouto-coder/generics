06513bde2f04720b14a7e5425916285f19b60589
Rafael Couto <rafaelsimoes133@gmail.com>
2022-07-26 23:02:16 -0300

    Add project files.

---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repository;
+
+var employeeRepository = new GenericRepository<Employee>();
+
+AddEmployees(employeeRepository);
+employeeRepository.Save();
+GetEmployeeById(employeeRepository);
+
+var organizationRepository = new GenericRepository<Organization>();
+
+AddOrganizations(organizationRepository);
+employeeRepository.Save();
+
+Console.ReadLine();
+
+void GetEmployeeById(GenericRepository<Employee> employeeRepository)
+{
+    var employee = employeeRepository.GetById(2);
+    Console.WriteLine($"Employee with id 2: {employee.FirstName}");
+}
+
+static void AddEmployees(GenericRepository<Employee> employeeRepository)
+{
+    employeeRepository.Add(new Employee { FirstName = "Bianca" });
+    employeeRepository.Add(new Employee { FirstName = "Anna" });
+    employeeRepository.Add(new Employee { FirstName = "Ruth" });
+}
+
+static void AddOrganizations(GenericRepository<Organization> organizationRepository)
+{
+    organizationRepository.Add(new Organization { Name = "Pluralsight" });
+    organizationRepository.Add(new Organization { Name = "Alura" });
+}
---
+++ WiredBrainCoffee.StorageApp/Repository/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Repository
+{
+    public class GenericRepository<T> where T : class, IEntity, new()
+    {
+        private readonly List<T> _items = new List<T>();
+
+        public T CreateItem()
+        {
+            return new T();
+        }
+
+        public T GetById(int id)
+        {
+            return _items.Single(item => item.Id == id);
+        }
+
+        public void Add(T item)
+        {
+            item.Id = _items.Any()
+                ? _items.Max(item => item.Id) + 1
+                : 1;
+
+            _items.Add(item);
+        }
+
+        public void Save()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void Save(T item)
+        {
+            _items.Remove(item);
+        }
+    }
+}
